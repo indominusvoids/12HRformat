58e9bf4dae0e78779e217af5c00637964434249f
indominusvoids <57304013+indominusvoids@users.noreply.github.com>
2024-10-10 11:10:20 -0500

    Add files via upload

---
+++ 12HRFormat/12HRFormat/Harmony/init.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace 12HRFormat
+{
+    public class Init : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
